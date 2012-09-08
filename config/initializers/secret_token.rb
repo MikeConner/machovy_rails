f410d142c8c59a3d3232a621169419a57948d02f
arash <arash.danaie@gmail.com>
2012-09-08 17:53:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MachovyRails::Application.config.secret_token = '8aa270de1a7381768bcdc4bc1d803bda0726aa4533dffa979f85045242212a7206f0fd592e6c3f9d19b3e89823cb1c688885b72884eef720aefbb81e032f8f4d'
