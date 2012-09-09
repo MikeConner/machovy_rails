c87a00e4ec452498b1b6c1ed1530689379b4e644
arash <arash.danaie@gmail.com>
2012-09-09 18:41:27 -0400

    rails_admin issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,9 @@ MachovyRails::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  # for heroku + Rails_admin
+  config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
