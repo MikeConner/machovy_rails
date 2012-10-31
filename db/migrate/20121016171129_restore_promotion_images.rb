b226954f3cbe5f4cc4c3816a9fa064b3e0a6d2e3
Bruce McElroy <bruce.mcelroy@gmail.com>
2012-10-31 19:18:31 -0400

    Resolved merge

--- db/migrate/20121016171129_restore_promotion_images.rb
+++ db/migrate/20121016171129_restore_promotion_images.rb
@@ -2,8 +2,8 @@ class RestorePromotionImages < ActiveRecord::Migration
  def change
 	  drop_table :promotion_images  if table_exists?(:promotion_images) 
     create_table :promotion_images do |t|
-      t.string :caption, :limit => PromotionImage::MAX_CAPTION_LEN
-      t.string :media_type, :limit => PromotionImage::MAX_TYPE_LEN
+      t.string :caption, :limit => 64
+      t.string :media_type, :limit => 64
       t.string :slideshow_image
       t.string :remote_image_url
       t.references :promotion
