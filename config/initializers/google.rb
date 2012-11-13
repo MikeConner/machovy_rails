653f0e34b63d42672b445750cbdc3ecc01c17610
arash <arash.danaie@gmail.com>
2012-11-13 12:25:13 -0500

    bad save of files for two initializers

--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -2,3 +2,4 @@ if ENV['GOOGLE_TA'].nil?
 	GOOGLE_TA = "44-1"
 else
   GOOGLE_TA = ENV['GOOGLE_TA']
+end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,9 +3,11 @@ if ENV['STRIPE_SECRET'].nil?
 	# "sk_0INin2jwTLiOY4xFw8wLZO3DWQWul"
 else
 	Stripe.api.key = ENV['STRIPE_SECRET']
+end
 
 if ENV['STRIPE_SECRET'].nil?
 	STRIPE_PUBLIC_KEY = "pl1"
 	# "pk_0INiaGmtnANOHmlgZKUD44MQ8Na41"
 else
   STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC']
+end
