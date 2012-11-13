03fa3b18f565b5be3042cfbc9f6d3bad7302b209
arash <arash.danaie@gmail.com>
2012-11-13 12:21:53 -0500

    Google TA, Stripe keys in Env variables

---
+++ config/initializers/google.rb
@@ -0,0 +1,4 @@
+if ENV['GOOGLE_TA'].nil?
+	GOOGLE_TA = "44-1"
+else
+  GOOGLE_TA = ENV['GOOGLE_TA']
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,11 @@
-Stripe.api_key = "sk_0INin2jwTLiOY4xFw8wLZO3DWQWul"
- 
-STRIPE_PUBLIC_KEY = "pk_0INiaGmtnANOHmlgZKUD44MQ8Na41"
+if ENV['STRIPE_SECRET'].nil?
+	Stripe.api_key = "44"
+	# "sk_0INin2jwTLiOY4xFw8wLZO3DWQWul"
+else
+	Stripe.api.key = ENV['STRIPE_SECRET']
+
+if ENV['STRIPE_SECRET'].nil?
+	STRIPE_PUBLIC_KEY = "pl1"
+	# "pk_0INiaGmtnANOHmlgZKUD44MQ8Na41"
+else
+  STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC']
