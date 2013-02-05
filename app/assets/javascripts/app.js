fb2046a0acd71ef0100997173c2d644ae4832eb7
arash <arash.danaie@gmail.com>
2013-02-04 19:54:26 -0500

    poc files

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -19,6 +19,8 @@
 
     $.fn.placeholder                ? $('input, textarea').placeholder() : null;
   });
+  
+  
 
   // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
   // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
