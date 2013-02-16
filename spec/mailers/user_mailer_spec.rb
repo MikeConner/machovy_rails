c3560a1ae7d4aa1a74ec5227c89dfd573d2f63cd
Jeff <endymionjkb@gmail.com>
2013-02-16 17:30:44 -0500

    Add delay notice in email
    Don't rely entirely on the merchants remembering to put it in the
    limitations.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -13,6 +13,55 @@ describe "UserMailer" do
     order.email.should == TEST_EMAIL
   end
 
+  describe "Promotion order email (delayed vouchers)" do
+    let(:order) { FactoryGirl.create(:order_with_delayed_vouchers) }
+    let(:msg) { UserMailer.promotion_order_email(order.reload) }
+    before do
+      order.promotion.limitations = FINE_PRINT
+      order.promotion.voucher_instructions = INSTRUCTIONS
+      order.promotion.save!
+    end
+    
+    it "should return a message object" do
+      msg.should_not be_nil
+    end
+  
+    it "should have the right sender" do
+      msg.from.to_s.should match(ApplicationHelper::MAILER_FROM_ADDRESS)
+    end
+    
+    describe "Send the message" do
+      before { msg.deliver }
+        
+      it "should get queued" do
+        ActionMailer::Base.deliveries.should_not be_empty
+        ActionMailer::Base.deliveries.count.should == 1
+      end
+      # msg.to is a Mail::AddressContainer object, not a string
+      # Even then, converting to a string gives you ["<address>"], so match captures the intent easier
+      it "should be sent to the right user" do
+        msg.to.to_s.should match(order.email)
+      end
+      
+      it "should have the right subject" do
+        msg.subject.should == UserMailer::ORDER_MESSAGE
+      end
+      
+      it "should have the right content" do
+        msg.body.encoded.should match('Thank you for your order')
+        msg.body.encoded.should match('See attachments for your voucher')
+        msg.body.encoded.should match('Cannot redeem for 6 hours after purchase')
+        msg.body.encoded.should match(FINE_PRINT)
+        msg.body.encoded.should match(INSTRUCTIONS)
+        order.vouchers.each do |voucher|
+          msg.body.encoded.should match(voucher.uuid + ".png")
+        end
+      
+        ActionMailer::Base.deliveries.count.should == 1
+      end
+    end
+  end  
+
   describe "Promotion order email" do
     let(:msg) { UserMailer.promotion_order_email(order.reload) }
     before do
