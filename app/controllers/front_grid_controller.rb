9df24b9fdf0a4eecdffe8e8fdfeeb97a4f449813
Jeff <endymionjkb@gmail.com>
2013-03-07 20:55:39 -0500

    Add Erotica feed, bug fixes
    Save gift certificates before mailing; update from Stripe; capture
    params in NewRelic

--- app/controllers/front_grid_controller.rb
+++ app/controllers/front_grid_controller.rb
@@ -2,6 +2,7 @@ require 'fixed_front_page_layout'
 
 class FrontGridController < ApplicationController
   MAX_PARTNER_VIEW_DEALS = 16
+  EROTICA_VENDOR_ID = 14
 
   def index
     # Need to have a metro, or filtering will return nothing
@@ -79,6 +80,17 @@ class FrontGridController < ApplicationController
     @target = nil
     
     render 'external_feed', :layout => 'layouts/machovy_feed'
+  end
+  
+  def erotica
+    @deals_per_row = Promotion::DEALS_PER_ROW
+    non_exclusive = Category.non_exclusive.map { |c| c.id }
+    @promotions = Promotion.front_page.select { |p| (p.displayable? or p.zombie? or p.coming_soon?) and (p.metro.name == 'Pittsburgh') and (p.vendor.id == EROTICA_VENDOR_ID) and !(p.category_ids & non_exclusive).empty? }.sort
+    if @promotions.length > MAX_PARTNER_VIEW_DEALS
+      @promotions = @promotions[0, MAX_PARTNER_VIEW_DEALS]
+    end
+
+    render 'midnightguru', :layout => 'layouts/affiliate'
   end
   
 private
