7329ac4a624b6d356672b2d84bf84ca609add714
Jeff <endymionjkb@gmail.com>
2013-03-24 22:13:52 -0400

    jQuery placeholder support; add single-column to layout
    Supposed to replace them in IE with something that works. Trigger
    mobile for blog posts at 1 column, not 2.

--- app/assets/javascripts/promotion.js
+++ app/assets/javascripts/promotion.js
@@ -18,6 +18,30 @@ $(function() {
     $.backstretch(["/assets/background-img.png"])
   }
   
+  jQuery(function() {
+	jQuery.support.placeholder = false;
+	test = document.createElement('input');
+	if('placeholder' in test) jQuery.support.placeholder = true;
+
+	if(!$.support.placeholder) { 
+		var active = document.activeElement;
+		$(':text').focus(function () {
+			if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+				$(this).val('').removeClass('hasPlaceholder');
+			}
+		}).blur(function () {
+			if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+				$(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+			}
+		});
+		$(':text').blur();
+		$(active).focus();
+		$('form').submit(function () {
+			$(this).find('.hasPlaceholder').each(function() { $(this).val(''); });
+		});
+	}
+  });
+    
   var $container = $('#container');
   $container.imagesLoaded( function(){
     $container.isotope({
